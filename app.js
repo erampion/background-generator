437e5ccca2ec139a6b77ac3785ad5e1cd6291bf6
erampion <ch7128@gmail.com>
2019-05-25 19:09:53 -0700

    adding files

---
+++ app.js
@@ -0,0 +1,54 @@
+const css = document.querySelector('#current-colors');
+const firstColor = document.querySelector('#first-color');
+const secondColor = document.querySelector('#second-color');
+const body = document.querySelector('#gradient');
+const randomBtn = document.querySelector('#random-btn');
+const buttonOne = document.querySelector('#btn-1');
+const buttonTwo = document.querySelector('#btn-2');
+
+// Event Listeners for the color inputs
+firstColor.addEventListener('input', setGradient);
+secondColor.addEventListener('input', setGradient);
+randomBtn.addEventListener('click', randomGradient);
+buttonOne.addEventListener('click', firstRandom);
+buttonTwo.addEventListener('click', secondRandom);
+
+
+// Call function to set the initial colors for the background
+setGradient();
+
+// Change the background colors
+function setGradient() {
+  body.style.background = `linear-gradient(to right, ${firstColor.value}, ${secondColor.value})`;
+  css.textContent = body.style.background;
+}
+
+// Random color
+function randomColor() {
+  let hexColorValues = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hexColorValues[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+// Generate both random colors
+function randomGradient() {
+  firstColor.value = randomColor();
+  secondColor.value = randomColor();
+  setGradient();
+}
+
+// Generate a random color on the left side
+function firstRandom() {
+  firstColor.value = randomColor();
+  setGradient();
+}
+
+// Generate a random color on the right side
+function secondRandom() {
+  secondColor.value = randomColor();
+  setGradient();
+}
+
